d3647fad46e843e80798944c547c880ddb9bd6c8
Dmitry Skrypka <dimitry.skripka@gmail.com>
2018-07-25 00:02:45 +0300

    clean

--- ui/server/app/routes/index.js
+++
@@ -1,5 +0,0 @@
-const recipe = require('../entities/recipe/recipe.routes');
-
-module.exports = app => {
-	app.use('/api/recipes', recipe);
-};
--- ui/server/config/db.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	dbname: 'cookbook-academy',
-	opts: {
-		autoReconnect: true
-	}
-};
